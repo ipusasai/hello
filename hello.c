07ebfdf230cba76e7b86bf6f9d2d5c4a7c0faaa2
k-sagun <skarki9@huskers.unl.edu>
2022-08-29 23:07:01 +0000

    Added partner name and major

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@
 int main(int argc, char **argv){
 
     printf("iris pudasaini ! \n");
+    printf("compuer science\n\n");
+
+    printf("Sagun Karki \n");
     printf("compuer science\n");
 
     return 0;
